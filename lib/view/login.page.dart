561086544f40e7a5b760ad0eec9d00bc8156a2dc
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-13 19:58:16 -0300

    adiciona lógica de consultar o usuário no `firestore`

--- lib/view/login.page.dart
+++ lib/view/login.page.dart
@@ -1,23 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  Future login(BuildContext context) async {
-    String email = emailController.text;
-    String password = passwordController.text;
+  bool isEmailFilled = false;
+  bool isPasswordFilled = false;
 
+  void onEmailFieldChange(String text) {
+    if (text.isNotEmpty) {
+      setState(() => isEmailFilled = true);
+    } else {
+      setState(() => isEmailFilled = false);
+    }
+  }
+
+  void onPasswordFieldChange(String text) {
+    if (text.isNotEmpty) {
+      setState(() => isPasswordFilled = true);
+    } else {
+      setState(() => isPasswordFilled = false);
+    }
+  }
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future login(BuildContext context) async {
     try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      String email = emailController.text;
+      String password = passwordController.text;
+
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+
+      String? id = auth.currentUser?.uid;
+
+      var user = await firestore.collection('users').doc(id).get();
+      var userRole = user.data()?['role'];
+
+      if (userRole == 'patient') {
+        // faz isso
+      } else if (userRole == 'therapist') {
+        // faz aquilo
+      }
+
       if (context.mounted) Navigator.of(context).pushNamed('/read-qr-code');
-    } catch (error) {
-      print(error);
+    } on FirebaseAuthException catch (error) {
+      print(error.code);
     }
   }
 
@@ -47,6 +85,7 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    onChanged: onEmailFieldChange,
                     controller: emailController,
                     decoration: InputDecoration(
                       hintText: "fulano@provedor.com",
@@ -58,6 +97,7 @@ class LoginPage extends StatelessWidget {
                   ),
                   SizedBox(height: 10),
                   TextField(
+                    onChanged: onPasswordFieldChange,
                     controller: passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
@@ -71,11 +111,18 @@ class LoginPage extends StatelessWidget {
                   SizedBox(height: 20),
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.4,
+
                     child: ElevatedButton(
                       onPressed:
-                          () async => await login(context),
+                          (isEmailFilled && isPasswordFilled)
+                              ? () async => await login(context)
+                              : null,
                       style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.blue),
+                        backgroundColor: WidgetStateProperty<Color?>.fromMap(
+                          <WidgetStatesConstraint, Color?>{
+                            ~WidgetState.disabled: Colors.black,
+                          },
+                        ),
                         padding: WidgetStateProperty.all(
                           EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                         ),
