21608ffc23519be923b23348f51610390639419a
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-17 11:18:35 -0300

    redireciona o usuário para tela inicial se ele já estiver logado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:socratize/firebase.options.dart';
@@ -18,28 +20,82 @@ void main() async {
       projectId: firebaseConfig['projectId']!,
       storageBucket: firebaseConfig['storageBucket']!,
       messagingSenderId: firebaseConfig['messagingSenderId']!,
-      appId: firebaseConfig['appId']!
-      ));
+      appId: firebaseConfig['appId']!,
+    ),
+  );
   runApp(SocratizeApp());
 }
 
-class SocratizeApp extends StatelessWidget {
+class SocratizeApp extends StatefulWidget {
   const SocratizeApp({super.key});
 
   @override
+  State<SocratizeApp> createState() => _SocratizeAppState();
+}
+
+class _SocratizeAppState extends State<SocratizeApp> {
+  late Future<String> _initialRouteFuture = initialRouteHandler();
+  final GlobalKey _appKey = GlobalKey();
+  final GlobalKey _loadingKey = GlobalKey();
+
+  Future<String> initialRouteHandler() async {
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+
+    if (uid == null) return "/login";
+
+    try {
+      var user = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      var userRole = user.data()!['role'];
+
+      switch (userRole) {
+        case 'patient':
+          return '/history';
+        case 'therapist':
+          return '/list-qr-codes';
+        default:
+          return '/login';
+      }
+    } catch (e) {
+      print(e);
+      return "/login";
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initialRouteFuture = initialRouteHandler();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Socratize',
-      theme: socratizeTheme,
-      home: Scaffold(body: LoginPage()),
-      routes: {
-        "/login": (context) => LoginPage(),
-        "/history": (context) => HistoryPage(),
-        "/chat": (context) => ChatPage(),
-        "/read-qr-code": (context) => ReadQRCodePage(),
-        "/gen-qr-code": (context) => GenQRCodePage(),
-        "/list-qr-codes": (context) => ListQRCodes(),
+    return FutureBuilder<String>(
+      future: _initialRouteFuture,
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (!snapshot.hasData) {
+          return MaterialApp(
+            key: _loadingKey,
+            debugShowCheckedModeBanner: false,
+            home: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        return MaterialApp(
+          key: _appKey,
+          debugShowCheckedModeBanner: false,
+          title: 'Socratize',
+          theme: socratizeTheme,
+          initialRoute: snapshot.data,
+          routes: {
+            "/login": (context) => LoginPage(),
+            "/history": (context) => HistoryPage(),
+            "/chat": (context) => ChatPage(),
+            "/read-qr-code": (context) => ReadQRCodePage(),
+            "/gen-qr-code": (context) => GenQRCodePage(),
+            "/list-qr-codes": (context) => ListQRCodes(),
+          },
+        );
       },
     );
   }
