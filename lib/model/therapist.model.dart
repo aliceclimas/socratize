b1da19a458a779f91d51f8aee91195d8df1c2dec
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-18 17:20:56 -0300

    implementa o model `Therapist`

--- lib/model/therapist.model.dart
+++ lib/model/therapist.model.dart
@@ -1,8 +1,20 @@
-class Therapist {
-  String id;
-  String name;
-  String email;
-  List<String> patients;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:socratize/model/user.model.dart';
 
-  Therapist(this.id, this.name, this.email, this.patients);
+class TherapistModel extends UserModel {
+  final List<String> patientsId;
+
+  TherapistModel({required super.fullname, required super.email, required super.role, required this.patientsId, super.id, super.active});
+
+  static TherapistModel fromDocument(
+    DocumentSnapshot<Map<String, dynamic>> document,
+  ) {
+    return TherapistModel(
+      fullname: document.data()?['name'],
+      email: document.data()?['email'],
+      role: document.data()?['role'],
+      patientsId: document.data()?['patientsId'],
+      active: document.data()?['active'],
+    );
+  }
 }
