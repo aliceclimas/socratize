74afd33be13820d3185c28c034c0e47ad81627a3
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-18 17:24:25 -0300

    refatora o código

--- lib/view/login.page.dart
+++ lib/view/login.page.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:socratize/model/user.model.dart';
@@ -15,55 +14,36 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  bool isEmailFilled = false;
-  bool isPasswordFilled = false;
-
-  void onEmailFieldChange(String email) {
-    (EmailValidator.validate(email)) ? setState(() => isEmailFilled = true) : setState(() => isEmailFilled = true);
-  }
-
-  void onPasswordFieldChange(String password) {
-      setState(() => isPasswordFilled = true);
-
-      if (password.length < 8) setState(() => isPasswordFilled = false);
-      if (!RegExp(r'[A-Z]').hasMatch(password)) setState(() => isPasswordFilled = false);
-      if (!RegExp(r'[a-z]').hasMatch(password)) setState(() => isPasswordFilled = false);
-      if (!RegExp(r'[0-9]').hasMatch(password)) setState(() => isPasswordFilled = false);
-      if (!RegExp(r'[\W_]').hasMatch(password)) setState(() => isPasswordFilled = false);
-      if (RegExp(r'\s').hasMatch(password)) setState(() => isPasswordFilled = false);
-      if (password.length > 100) setState(() => isPasswordFilled = false);
-  }
-
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-  Future login(BuildContext context) async {
+  Future<void> login(BuildContext context) async {
     try {
       String email = emailController.text;
       String password = passwordController.text;
       // falta validar se o e-mail é válido e se a senha é forte
 
       // autentica o usuário
-      await auth.signInWithEmailAndPassword(email: email, password: password);
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
 
       // lógica para verificar se é terapeuta ou paciente
-      String? id = auth.currentUser?.uid;
-      DocumentSnapshot<Map<String, dynamic>> userDoc =
-          await firestore.collection('users').doc(id).get();
-      UserModel user = UserModel.fromDocument(userDoc);
+      String? id = FirebaseAuth.instance.currentUser?.uid;
+      var userDoc = (await FirebaseFirestore.instance.collection('users').doc(id).get());
+
+      UserModel userModel = UserModel.fromDocument(userDoc);
+
+      if (!context.mounted) throw Exception('Erro interno');
+
+      String route = "/login";
+      switch (userModel.role) {
+        case 'patient': route = '/history';
+        case 'therapist': route = '/list-qr-codes';
+
+      }
+
+      Navigator.of(context).pushReplacementNamed(route);
 
       // navegação dinâmica
-      if (context.mounted) {
-        if (user.role == 'patient') {
-          if (user.active!) {
-            Navigator.of(context).pushNamed('/history');
-          } else {
-            Navigator.of(context).pushNamed('/read-qr-code');
-          }
-        } else if (user.role == 'therapist') {
-          Navigator.of(context).pushNamed('/list-qr-codes');
-        }
-      }
     } on FirebaseAuthException catch (error) {
       if (context.mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -107,7 +87,6 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
                 children: [
                   TextField(
-                    onChanged: onEmailFieldChange,
                     controller: emailController,
                     decoration: InputDecoration(
                       hintText: "fulano@provedor.com",
@@ -119,7 +98,6 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   SizedBox(height: 10),
                   TextField(
-                    onChanged: onPasswordFieldChange,
                     controller: passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
@@ -135,16 +113,9 @@ class _LoginPageState extends State<LoginPage> {
                     width: MediaQuery.of(context).size.width * 0.4,
 
                     child: ElevatedButton(
-                      onPressed:
-                          (isEmailFilled && isPasswordFilled)
-                              ? () async => await login(context)
-                              : null,
+                      onPressed: () async => await login(context),
                       style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty<Color?>.fromMap(
-                          <WidgetStatesConstraint, Color?>{
-                            ~WidgetState.disabled: Colors.blue,
-                          },
-                        ),
+                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
                         padding: WidgetStateProperty.all(
                           EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                         ),
