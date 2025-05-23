ab772520353c1e8d4b92ed2fb05ffe176f8bc324
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-23 14:32:09 -0300

    arruma o overflow do teclado

--- lib/view/login.page.dart
+++ lib/view/login.page.dart
@@ -34,7 +34,6 @@ class _LoginPageState extends State<LoginPage> {
       switch (userModel.role) {
         case 'patient': route = '/history';
         case 'therapist': route = '/list-qr-codes';
-
       }
 
       Navigator.of(context).pushReplacementNamed(route);
@@ -60,88 +59,92 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage('assets/images/socratize-logo-nome.png'),
-              width: MediaQuery.of(context).size.width * 0.5,
-              height: MediaQuery.of(context).size.width * 0.5,
-            ),
-            Text(
-              "Bem-vindo(a)",
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text(
-              "Vamos pensar juntos sobre o que está te incomodando?",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 50),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              child: Column(
-                children: [
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: "fulano@provedor.com",
-                      prefixIcon: Icon(Icons.mail),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "senhasegura",
-                      prefixIcon: Icon(Icons.password),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.4,
-
-                    child: ElevatedButton(
-                      onPressed: () async => await login(context),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
-                        padding: WidgetStateProperty.all(
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        ),
-                        shape: WidgetStateProperty.all(
-                          RoundedRectangleBorder(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage('assets/images/socratize-logo-nome.png'),
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.width * 0.5,
+                ),
+                Text(
+                  "Bem-vindo(a)",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                Text(
+                  "Vamos pensar juntos sobre o que está te incomodando?",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 50),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          hintText: "fulano@provedor.com",
+                          prefixIcon: Icon(Icons.mail),
+                          border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
                         ),
                       ),
-                      child: Text(
-                        "ENTRAR",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                          fontSize: 20,
+                      SizedBox(height: 10),
+                      TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "senhasegura",
+                          prefixIcon: Icon(Icons.password),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
                         ),
                       ),
-                    ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+
+                        child: ElevatedButton(
+                          onPressed: () async => await login(context),
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                            padding: WidgetStateProperty.all(
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                            ),
+                            shape: WidgetStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            "ENTRAR",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed:
+                            () => {
+                              Navigator.of(context).pushNamed('/read-qr-code'),
+                            },
+                        child: Text("É seu primeiro acesso?"),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed:
-                        () => {
-                          Navigator.of(context).pushNamed('/read-qr-code'),
-                        },
-                    child: Text("É seu primeiro acesso?"),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
