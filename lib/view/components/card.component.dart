2f0b2dafaa7e4ab5134a0889bf22559119679a62
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-30 20:37:22 -0300

    refatora o código deixando mais organizado

--- lib/view/components/card.component.dart
+++ lib/view/components/card.component.dart
@@ -1,41 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:socratize/model/cognitive_disfunction.model.dart';
 
 class InsightCard extends StatelessWidget {
-  final String tipo;
-  final String frase;
+  InsightCard({super.key, required this.name, required this.cognitiveDisfunctionName});
 
-  const InsightCard({super.key, required this.tipo, required this.frase});
-
-  static const Map<String, String> explicacoes = {
-    "PERSONALIZAÇÃO":
-        " Assumir responsabilidade por tudo que acontece, mesmo quando não é culpa sua.",
-    "FILTRO MENTAL":
-        " Focar apenas nos aspectos negativos de uma situação e ignorar os positivos.",
-    "GENERALIZAÃO EXCESSIVA":
-        " Tirar conclusões amplas a partir de um único evento negativo, acreditando que 'sempre' será assim.",
-    "CATASTROFIZAÇÃO":
-        " Imaginar que o pior cenário possível vai ocorrer, exagerando as consequências de um evento.",
-    "PENSAMENTO DICÓTOMO":
-        "(Tudo ou nada) Ver as situações de forma extrema, sem considerar que há pontos intermediários (tudo é perfeito ou um desastre total).",
-    "LEITURA DA MENTE":
-        " Pressumir saber o que os outros estão pensando sem ter provas concretas.",
-    "RACIOCÍNIO EMOCIONAL":
-        "Considerar que, se você sente algo interessante, essa emoção reflete a verdade absoluta da situação.",
-    "DESQUALIFICAÇÃO DO POSITIVO":
-        "Descartar ou minimizar suas conquistas e os feedbacks positivos, focando somente nos erros.",
-    "USO DE DEVERIA":
-        "Impor regras rígicas sobre como as coisas 'devem' ser, levando à frustação quando a realizada não se encaixa nesses padrões.",
+  final Map<String, CognitiveDisfunction> cognitiveDisfunctionMap = {
+    "personalizacao": CognitiveDisfunction(title: "Personalização" , description: "Assumir responsabilidade por tudo que acontece, mesmo quando não é culpa sua."),
+    "filtro-mental": CognitiveDisfunction(title: "Filtro Mental" , description:  "Focar apenas nos aspectos negativos de uma situação e ignorar os positivos.",),
+    "generalizacao-excecssiva": CognitiveDisfunction(title: "Generalização Excessiva" , description: "Tirar conclusões amplas a partir de um único evento negativo, acreditando que 'sempre' será assim."),
+    "catastrofizacao": CognitiveDisfunction(title: "Catastrofização" , description: "Imaginar que o pior cenário possível vai ocorrer, exagerando as consequências de um evento."),
+    "pensamento-dicotomico": CognitiveDisfunction(title: "Pensamento Dicotômico" , description: "(Tudo ou nada) Ver as situações de forma extrema, sem considerar que há pontos intermediários (tudo é perfeito ou um desastre total)."),
+    "leitura-mental": CognitiveDisfunction(title: "Leitura Mental" , description: "Pressumir saber o que os outros estão pensando sem ter provas concretas."),
+    "desqualificacao-positivo": CognitiveDisfunction(title: "Desqualificação do Positivo" , description: "Descartar ou minimizar suas conquistas e os feedbacks positivos, focando somente nos erros."),
+    "raciocinio-emocional": CognitiveDisfunction(title: "Raciocinío Emocional" , description: "Considerar que, se você sente algo interessante, essa emoção reflete a verdade absoluta da situação."),
+    "uso-de-deveria": CognitiveDisfunction(title: "Uso de Deveria" , description: "Impor regras rígicas sobre como as coisas 'devem' ser, levando à frustação quando a realizada não se encaixa nesses padrões.")
   };
 
-  void mostrarExplicacao(BuildContext context) {
-    final texto =
-        explicacoes[tipo.toUpperCase()] ?? 'Explicação não encontrada.';
+  final String name;
+  final String cognitiveDisfunctionName; // campor idDisfuncaoCogntiva do Questionamento
+
+  void showDisfunctionInfo(BuildContext context) {
+    final CognitiveDisfunction cognitiveDisfunction = cognitiveDisfunctionMap[cognitiveDisfunctionName]!;
+
     QuickAlert.show(
       context: context,
       type: QuickAlertType.info,
-      title: tipo,
-      text: texto,
+      title: cognitiveDisfunction.title,
+      text: cognitiveDisfunction.description,
       confirmBtnText: 'Fechar',
     );
   }
@@ -52,7 +44,7 @@ class InsightCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(child: Text(frase, style: TextStyle(fontSize: 16))),
+                Expanded(child: Text(name, style: TextStyle(fontSize: 16))),
                 TextButton(
                   onPressed:
                       () => Navigator.of(
@@ -74,7 +66,7 @@ class InsightCard extends StatelessWidget {
             const SizedBox(height: 8),
             // Rótulo com o tipo do pensamento
             TextButton(
-              onPressed: () => mostrarExplicacao(context),
+              onPressed: () => showDisfunctionInfo(context),
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all(Color(0xffCDD3FF)),
               ),
@@ -84,7 +76,7 @@ class InsightCard extends StatelessWidget {
                   Icon(Icons.bookmark, color: Color(0xff5975FF)),
                   SizedBox(width: 5),
                   Text(
-                    tipo.toUpperCase(),
+                    cognitiveDisfunctionMap[cognitiveDisfunctionName]!.title.toUpperCase(),
                     style: TextStyle(color: Color(0xff5975FF)),
                   ),
                 ],
